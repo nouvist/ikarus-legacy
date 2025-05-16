a77dc6b96ed4034306c8b0b98b3929ff25955ac4
nouvist <13176878+nouvist@users.noreply.github.com>
2025-05-17 02:01:26 +0700

    feat: add reactjs

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,41 @@
 import { app, BrowserWindow } from "electron";
-import path from "node:path";
 import started from "electron-squirrel-startup";
+import path from "node:path";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (started) {
-  app.quit();
-}
+if (started) app.quit();
 
-const createWindow = () => {
+function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  const win = new BrowserWindow({
+    frame: false,
+    roundedCorners: true,
+    fullscreenable: false,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      devTools: !!MAIN_WINDOW_VITE_DEV_SERVER_URL,
+      webviewTag: true,
     },
   });
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    win.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(
-      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+    win.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
     );
   }
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+  win.webContents.on("before-input-event", (event, input) => {
+    const disabledKeys = [
+      input.control && input.code === "KeyR",
+      input.code === "F5",
+    ];
+
+    if (disabledKeys.some(Boolean)) event.preventDefault();
+  });
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -39,18 +46,11 @@ app.on("ready", createWindow);
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  if (process.platform !== "darwin") app.quit();
 });
 
 app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -26,8 +26,13 @@
  * ```
  */
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+  </StrictMode>
 );
